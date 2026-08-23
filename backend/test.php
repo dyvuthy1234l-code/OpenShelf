8d75891dd261b8897496cf53a8980a068530ffb7
Vu Thy <jjjj29880@gmail.com>
2026-08-23 20:52:12 +0700

    Fix test.php syntax and clean up test files

--- backend/test.php
+++ backend/test.php
@@ -1,13 +1,23 @@
 <?php
-\ = 'jsyajhtr';
-\ = '344373826751911';
-\ = 'Ukxlt0ykALh9OP1Jq0KKfN0A_xY';
-\ = time();
-\ = sha1('timestamp=' . \ . \);
-\ = curl_init('https://api.cloudinary.com/v1_1/' . \ . '/image/upload');
-curl_setopt(\, CURLOPT_RETURNTRANSFER, true);
-curl_setopt(\, CURLOPT_POST, true);
-curl_setopt(\, CURLOPT_POSTFIELDS, ['file' => 'https://picsum.photos/200', 'api_key' => \, 'timestamp' => \, 'signature' => \]);
-\ = curl_exec(\);
-echo \;
+
+$cloudName = 'jsyajhtr';
+$apiKey = '524473598627689';
+$apiSecret = 'oBXG8v6i-3eXmqPISNcZVm2dIms';
+$timestamp = time();
+$signature = sha1('timestamp=' . $timestamp . $apiSecret);
+
+$ch = curl_init('https://api.cloudinary.com/v1_1/' . $cloudName . '/image/upload');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, [
+    'file' => 'https://picsum.photos/200',
+    'api_key' => $apiKey,
+    'timestamp' => $timestamp,
+    'signature' => $signature,
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
+
 
